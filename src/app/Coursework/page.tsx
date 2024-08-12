23ece39492b8396f9629c65a5d4f8f4e44642097
HaileyEmery <haileyemery02@gmail.com>
2024-08-11 21:02:19 -0600

    style changes and formatting fixes to main pages

--- src/app/Coursework/page.tsx
+++ src/app/Coursework/page.tsx
@@ -2,47 +2,49 @@ let CourseNameFormat="font-bold text-3xl md:text-3xl lg:text-5xl"
 export default function Coursework() {
     return (
       <div className="flex flex-col gap-y-10">
-      <div>
-      <p className="font-bold text-5xl md:text-7xl lg:text-9xl ">Relevent Coursework</p>
-      </div><div>
-      <br></br>
-        <p className={CourseNameFormat}>Software Testing</p>
-        <p className="font-semibold text-l md:text-xl lg:text-2xl">College of Natural Sciences - Spring 2024</p>
-        <p className="text-l md:text-xl lg:text-2xl"></p>
         <br></br>
-        <p className={CourseNameFormat}>Database Systems</p>
-        <p className="font-semibold text-l md:text-xl lg:text-2xl">College of Natural Sciences - Spring 2024</p>
-        <p className="text-l md:text-xl lg:text-2xl"></p>
+        <div>
+          <p className="font-bold text-5xl md:text-7xl lg:text-9xl ">Relevant Coursework</p>
+        </div>
+        <div>
         <br></br>
-        <p className={CourseNameFormat}>Senior Design Project</p>
-        <p className="font-semibold text-l md:text-xl lg:text-2xl">College of Engineering - Fall 2023 & Spring 2024</p>
-        <p className="text-l md:text-xl lg:text-2xl"></p>
-        <br></br>
-        <p className={CourseNameFormat}>Computer Networks and the Internet</p>
-        <p className="font-semibold text-l md:text-xl lg:text-2xl">College of Natural Sciences - Fall 2023</p>
-        <p className="text-l md:text-xl lg:text-2xl"></p>
-        <br></br>
-        <p className={CourseNameFormat}>Embedded Systems and Machine Learning</p>
-        <p className="font-semibold text-l md:text-xl lg:text-2xl">College of Natural Sciences - Fall 2023</p>
-        <p className="text-l md:text-xl lg:text-2xl"></p>
-        <br></br>
-        <p className={CourseNameFormat}>Modern CyberSecurity</p>
-        <p className="font-semibold text-l md:text-xl lg:text-2xl">College of Natural Sciences - Fall 2023</p>
-        <p className="text-l md:text-xl lg:text-2xl"></p>
-        <br></br>
-        <p className={CourseNameFormat}>Operating Systems</p>
-        <p className="font-semibold text-l md:text-xl lg:text-2xl">College of Natural Sciences - Spring 2023</p>
-        <p className="text-l md:text-xl lg:text-2xl"></p>
-        <br></br>
-        <p className={CourseNameFormat}>Linear System Analysis II</p>
-        <p className="font-semibold text-l md:text-xl lg:text-2xl">College of Engineering - Spring 2023</p>
-        <p className="text-l md:text-xl lg:text-2xl"></p>
-        <br></br>
-        <p className={CourseNameFormat}>Computer Organization and Architecture</p>
-        <p className="font-semibold text-l md:text-xl lg:text-2xl">College of Engineering - Spring 2023</p>
-        <p className="text-l md:text-xl lg:text-2xl"></p>
-        <br></br>
-      </div>
+          <p className={CourseNameFormat}>Software Testing</p>
+          <p className="font-semibold text-l md:text-xl lg:text-2xl">College of Natural Sciences - Spring 2024</p>
+          <p className="text-l md:text-xl lg:text-2xl"></p>
+          <br></br>
+          <p className={CourseNameFormat}>Database Systems</p>
+          <p className="font-semibold text-l md:text-xl lg:text-2xl">College of Natural Sciences - Spring 2024</p>
+          <p className="text-l md:text-xl lg:text-2xl"></p>
+          <br></br>
+          <p className={CourseNameFormat}>Senior Design Project</p>
+          <p className="font-semibold text-l md:text-xl lg:text-2xl">College of Engineering - Fall 2023 & Spring 2024</p>
+          <p className="text-l md:text-xl lg:text-2xl"></p>
+          <br></br>
+          <p className={CourseNameFormat}>Computer Networks and the Internet</p>
+          <p className="font-semibold text-l md:text-xl lg:text-2xl">College of Natural Sciences - Fall 2023</p>
+          <p className="text-l md:text-xl lg:text-2xl"></p>
+          <br></br>
+          <p className={CourseNameFormat}>Embedded Systems and Machine Learning</p>
+          <p className="font-semibold text-l md:text-xl lg:text-2xl">College of Natural Sciences - Fall 2023</p>
+          <p className="text-l md:text-xl lg:text-2xl"></p>
+          <br></br>
+          <p className={CourseNameFormat}>Modern CyberSecurity</p>
+          <p className="font-semibold text-l md:text-xl lg:text-2xl">College of Natural Sciences - Fall 2023</p>
+          <p className="text-l md:text-xl lg:text-2xl"></p>
+          <br></br>
+          <p className={CourseNameFormat}>Operating Systems</p>
+          <p className="font-semibold text-l md:text-xl lg:text-2xl">College of Natural Sciences - Spring 2023</p>
+          <p className="text-l md:text-xl lg:text-2xl"></p>
+          <br></br>
+          <p className={CourseNameFormat}>Linear System Analysis II</p>
+          <p className="font-semibold text-l md:text-xl lg:text-2xl">College of Engineering - Spring 2023</p>
+          <p className="text-l md:text-xl lg:text-2xl"></p>
+          <br></br>
+          <p className={CourseNameFormat}>Computer Organization and Architecture</p>
+          <p className="font-semibold text-l md:text-xl lg:text-2xl">College of Engineering - Spring 2023</p>
+          <p className="text-l md:text-xl lg:text-2xl"></p>
+          <br></br>
+        </div>
       </div>
   );
 }
--- src/app/Resume/page.tsx
+++ src/app/Resume/page.tsx
@@ -1,10 +1,12 @@
 export default function Page() {
     return (
       <div className="flex flex-col gap-y-10">
-      <div>
-      <p className="font-bold text-5xl md:text-7xl lg:text-9xl "> Resume</p>
-      </div><div>
-      </div>
+        <br></br>
+        <div>
+          <p className="font-bold text-5xl md:text-7xl lg:text-9xl "> Resume</p>
+        </div>
+        <div>
+        </div>
       </div>
   );
 }
