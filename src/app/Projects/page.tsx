d4f480b9ab136506e1c6063bddcfca6374feee1d
HaileyEmery <haileyemery02@gmail.com>
2024-08-30 14:49:39 -0600

    Add the Vision Breaks Project (Windows) to the Projects page

--- src/app/Projects/page.tsx
+++ src/app/Projects/page.tsx
@@ -1,4 +1,28 @@
 import {formatting} from '../formatting.tsx'
+
+let visionBreaksWindowsCode = `from time import sleep
+from os import startfile, system
+from ctypes import windll
+
+SCREENSAVER_PATH = "C:\\Windows\\System32\\"
+SCREENSAVER_NAME ="Ribbons.scr"
+BREAK_DURATION_SECONDS = 30
+BREAK_FREQUENCY_MINUTES = 20
+
+def StartBreak():
+    windll.user32.BlockInput(True) #Start Blocking Keyboard and Mouse Input
+    startfile(SCREENSAVER_PATH + SCREENSAVER_NAME) #Start Screen Saver
+
+def EndBreak():
+    system("taskkill /f /im " + SCREENSAVER_NAME) #Close Screensaver
+    windll.user32.BlockInput(False) #Stop Blocking Keyboard and Mouse Input
+
+while True:
+    sleep(BREAK_FREQUENCY_MINUTES*60 - BREAK_DURATION_SECONDS)
+    StartBreak()
+    sleep(BREAK_DURATION_SECONDS)
+    EndBreak()`
+
 export default function Projects() {
   return (<>
     <p className={formatting.title}>
@@ -19,6 +43,22 @@ export default function Projects() {
       <p className={formatting.normal}>
         A python based application that automatically configures large scale networks to run Zero Touch Provisioning.
       </p>
+    </div>
+    <div>
+      <p className={formatting.heading1}>
+        Vision Breaks Project
+      </p>
+      <p className={formatting.heading2}>
+        Windows
+      </p>
+      <p className={formatting.normal}>
+        A python script that opens the screensaver for 30 seconds every twenty minutes to provide the oppurtunity for regular vision breaks. 
+      </p>
+      <pre className={formatting.normal}>
+        <code>
+          {visionBreaksWindowsCode}
+        </code>
+      </pre>
     </div>
   </>);
 }
